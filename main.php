f45a197bf38e3657a92821dc4709bce8486af02a
PAPYmoujo2104 <43677349+PAPYmoujo2104@users.noreply.github.com>
2018-09-30 21:29:12 +0200

    Add files via upload

---
+++ main.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <title>Rituel.com</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+
+<body>
+	<!--Titre-->
+<?php include("include/header.php"); ?>
+
+	<section>
+		<article>
+			<div href="main.html">
+				<figure>
+    				<img src="image/sweat.jpg" alt="sweat" />
+    				<figcaption>SWEAT</figcaption>
+    			</figure>
+			</div>
+		</article>
+
+		<article>
+			<div href="main.html">
+				<figure>
+    				<img src="image/pull.jpg" alt="pull" />
+    				<figcaption>PULL</figcaption>
+    			</figure>
+			</div>
+		</article>
+
+		<article>
+			<div href="main.html">
+				<figure>
+    				<img src="image/tshirt.jpg" alt="tshirt" />
+    				<figcaption>T-SHIRT</figcaption>
+    			</figure>
+			</div>
+		</article>
+
+	</section>
+
+	<footer>
+			
+	</footer>
+
+</body>
+</html>
